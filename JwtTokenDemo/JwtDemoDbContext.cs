a5ad7094afccf7fbfb5ebef8ccd418e2774b4638
Marius Paškevičius <marius.paskevicius@yahoo.com>
2023-02-02 18:48:34 +0200

    Add project files.

---
+++ JwtTokenDemo/JwtDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JwtTokenDemo
+{
+    public class JwtDemoDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public JwtDemoDbContext(DbContextOptions<JwtDemoDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
