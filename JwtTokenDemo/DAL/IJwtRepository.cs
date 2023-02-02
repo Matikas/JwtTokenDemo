8a0c7afd717cbd15d92f8b611073090a6fbdef94
Marius Paškevičius <marius.paskevicius@yahoo.com>
2023-02-02 18:59:18 +0200

    repository implementation

---
+++ JwtTokenDemo/DAL/IJwtRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtTokenDemo.DAL
+{
+    public interface IJwtRepository
+    {
+        void SaveAccount(Account account);
+        Account GetAccount(string username);
+    }
+}
---
+++ JwtTokenDemo/DAL/JwtRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace JwtTokenDemo.DAL
+{
+    public class JwtRepository : IJwtRepository
+    {
+        private readonly JwtDemoDbContext _context;
+        public JwtRepository(JwtDemoDbContext context)
+        {
+            _context = context;
+        }
+
+        public Account GetAccount(string username)
+        {
+            return _context.Accounts.FirstOrDefault(a => a.Username == username);
+        }
+
+        public void SaveAccount(Account account)
+        {
+            _context.Accounts.Add(account);
+            _context.SaveChanges();
+        }
+    }
+}
